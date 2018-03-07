3a5b08d9daca447546ad2e2aebfcf5da0dfd1eed
crickett <crickettcode@gmail.com>
2018-03-06 20:27:59 -0500

    gamer

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log("Up and running!");
+var cardOne="queen";
+var cardTwo="queen";
+var cardThree="king";
+var cardFour="king";
+
+
+var cards= ['queen', 'queen', 'king','king'];
+var cardsInPlay= []
+var='cardOne';
+.push(cardOne);
+
+console.log("User flipped" + " " + cardOne);
+
+alert('Hello, friends.'); 
