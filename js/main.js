391d5d6431af0d6d0481371c88d81b2b25013f8a
crickett <crickettcode@gmail.com>
2018-03-07 10:54:17 -0500

    logic edits

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 
 console.log("Up and running!");
 
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + "cardOne");
+console.log("User flipped " + "cardTwo");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
 
  
+var cards = new Array("queen","queen","king","king");
+
+var cardsInPlay = new Array ();
+document.write(cards[0]);
+
+
+var cardOne = cards[0];
+cardsInPlay.push("cardOne");
+var cardTwo = cards[2]
+cardsInPlay.push("cardTwo");
+
+if 
+
+
+
+
+
+
+
+
+
