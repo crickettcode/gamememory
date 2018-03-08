8556f21d7abd1c898aa37c8db6065eff52f518f7
crickett <crickettcode@gmail.com>
2018-03-07 21:48:52 -0500

    created game logic2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
 console.log("Up and running!");
 
-var cardOne= "queen";
-var cardTwo= "queen";
-var cardThree="king";
-var cardFour="king";
-
-
 console.log("User flipped" + cardOne);
 console.log("User flipped" +  cardFour);
 
 
 
+alert('Hello, friends.');
+
+var cards=['queen','queen','king','king'];
+
+var cardsInPlay=[];
+
+var cardOne= cards[0];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped" + cardOne);
+
+
+var cardFour = cards[3];
+
+cardsInPlay.push(cardFour);
+console.log("User flipped" + cardFour); 
+
+if (cardsInPlay.length===3) {
 
 
 
-
-
+if (cardsInPlay[0]===cardsInPlay[1])
+{
+alert ("You found a match!");
+}
+ else; 
+ {
+	alert("Sorry, try again.");
+ }
 
 
