ff1574f5f8a9a305010c027a84398c5c58726253
crickett <crickettcode@gmail.com>
2018-03-07 20:12:38 -0500

    created game logic2

--- js/main.js
+++ js/main.js
@@ -1,28 +1,14 @@
-
 console.log("Up and running!");
 
-
-console.log("User flipped " + "cardOne");
-console.log("User flipped " + "cardTwo");
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
- 
-var cards = new Array("queen","queen","king","king");
-
-var cardsInPlay = new Array ();
-document.write(cards[0]);
+var cardOne= "queen";
+var cardTwo= "queen";
+var cardThree="king";
+var cardFour="king";
 
 
-var cardOne = cards[0];
-cardsInPlay.push("cardOne");
-var cardTwo = cards[2]
-cardsInPlay.push("cardTwo");
+console.log("User flipped" + cardOne);
+console.log("User flipped" +  cardFour);
 
-if 
 
 
 
